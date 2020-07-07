95ccf853a35e814a16d9e3114fb06041d4a07fbc
mrzepecki <m.rzepaa@gmail.com>
2020-07-07 14:31:27 +0200

    Update

--- LinqFull/Program.cs
+++ LinqFull/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace LinqFull
 {
     class Program
     {
+        static bool isThereFull (string tab)
+        {
+            var full = from figure in tab
+                       where char.IsLetterOrDigit(figure)
+                       group figure by figure
+                       into figureGroup
+                       select figureGroup;
+
+            if (full.Count() > 2)
+                return false;
+                
+            foreach (var group in full)
+                if (group.Count() == 1 || group.Count() == 0)
+                    return false;
+
+            return true;
+        }
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string data;
+            using (FileStream input = File.OpenRead("input.txt"))
+            using (StreamReader reader = new StreamReader(input))
+            {
+                 data = reader.ReadToEnd(); 
+            }
+
+            string[] hands = data.Split(";");
+           
+            foreach (var item in hands)
+            {
+                if (isThereFull(item))
+                    Console.WriteLine(item);
+            }
+            
         }
     }
 }
